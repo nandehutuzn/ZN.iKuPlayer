6213461b5d5ba447e1534452cceecdde8e10a34d
nandehutuzn <120902259@qq.com>
2016-12-07 22:50:38 +0800

    2016/12/07 22:50

---
+++ ZN.iKuPlayer/ZN.iKuPlayer.WPF/Modules/Model/SearchSong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZN.iKuPlayer.WPF.Modules.Model
+{
+    /// <summary>
+    /// 搜索到的歌曲
+    /// </summary>
+    class SearchSong
+    {
+        private string _song;
+        /// <summary>
+        /// 歌名
+        /// </summary>
+        public string Song {
+            get { return _song; }
+            set { _song = value; }
+        }
+
+        private string _singer;
+        /// <summary>
+        /// 歌手名
+        /// </summary>
+        public string Singer{
+            get { return _singer; }
+            set { _singer = value; }
+        }
+    }
+}
---
+++ ZN.iKuPlayer/ZN.iKuPlayer.WPF/Modules/Model/Searcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using ZN.Dotnet.Tools;
+
+namespace ZN.iKuPlayer.WPF.Modules.Model
+{
+    /// <summary>
+    /// 专门用于搜索歌曲类
+    /// </summary>
+    class Searcher
+    {
+        /// <summary>
+        /// 歌曲搜索URL
+        /// </summary>
+        private static string _searchUrl = @"";
+    }
+}
